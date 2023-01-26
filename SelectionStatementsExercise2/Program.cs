6b6d6d72fa9d82cbe6f5ae23f8f5130cc81709dc
Alen Valdez <alenkvaldeztch@gmail.com>
2023-01-26 15:36:37 -0800

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace SelectionStatementsExercise2
+﻿using Internal;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("whats your fave school subject?");
+
+            var subjcet = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Concepts in Rehabalitiatino":
+                    Console.WriteLine("concepts is not that bad");
+                    break;
+                case "Clinical Conditions and Applications"
+                    Console.WriteLine("Condition and apps is a little bad");
+                    break;
+                case "Neuro for OT"
+                    Console.WriteLine("pretty bad");
+                default:
+                    Console.WriteLine("i have never heard of that subject in my life what is that. {subject} sounds ok though");
+                case "Professional Practice"
+                    Console.WriteLine("free class yes");
+                    break;
+            }
         }
     }
 }
